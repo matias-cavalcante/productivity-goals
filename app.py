b2f006fcc6ddfa66e14eac78786e1899125cf4ca
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-11-12 21:29:55 +0000

    Insert acitivity endpoint and function in API work well

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
-from requests import request
+from flask import Flask, request, jsonify
+from databaseOperations import ActivitiesOperations
 
 app = Flask(__name__)
 
+activities_ops = ActivitiesOperations()
 
-@app.route('/activitiescreate')
-# ‘/’ URL is bound with hello_world() function.
+
+@app.route('/createactivity', methods=['POST'])
 def create_activity():
-    activity = request.json()
-    print(request)
-    return 'Hello World'
+    """Receives and passes an activity to be inserted in the activities collection"""
+    data = request.get_json() or {}
+    activity = data.get('activity')
+    # print(f"Received activity: {activity}")
+    activity_id = activities_ops.insert_activity(activity)
+    return jsonify({"message": "Activity received", "activity": activity}), 200
+
+
+@app.route('/seeactivities', methods=['GET'])
+def show_activities():
+    activities = activities_ops.get_all_activities()
+    return jsonify({"activities": activities}), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
