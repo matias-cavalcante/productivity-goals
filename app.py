0d840c889a31cba8639f308a6b56a44f034d0418
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-11-12 23:28:18 +0000

    The function to create new challenges has been added to the flask app

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify
-from databaseOperations import ActivitiesOperations
+from databaseOperations import ActivitiesOperations, ChallengesOperations
 from bson import json_util  # Import json_util from pymongo
+from datetime import datetime, timedelta
 
 
 app = Flask(__name__)
 
 activities_ops = ActivitiesOperations()
+challenges_ops = ChallengesOperations()
+
+# ******************** ACTIVITIES ENDPOINTS ********************
 
 
 @app.route('/createactivity', methods=['POST'])
@@ -26,6 +30,23 @@ def show_activities():
     activities_json = json_util.dumps(activities)
     # print(activities_json)
     return jsonify(activities_json), 200
+
+# ******************** CHALLENGES ENDPOINTS  ********************
+
+
+def new_challenge_prepare_data(enddate):
+    """It is used by new_challenge to feed the data in the right shape to the create_challenge method"""
+    return {'start_date': datetime.now(), 'end_date': datetime.now() + timedelta(days=int(enddate)), 'checks': {}}
+
+
+@app.route('/createchallenge', methods=['POST'])
+def new_challenge():
+    """Receives a total of days and using that data a challenge is created and with create_challenge inserted in db"""
+    data = request.get_json()
+    total_days = data.get('days')
+    challenge_data = new_challenge_prepare_data(total_days)
+    new_challenge = challenges_ops.create_challenge(challenge_data)
+    return jsonify({"message": "Challenge received", "total days": total_days}), 200
 
 
 if __name__ == '__main__':
