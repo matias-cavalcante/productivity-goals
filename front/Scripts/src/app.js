ddee64a7302fa452d7a98c263f5df56d9038b789
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-11-18 22:16:57 +0000

    Added to my TS file, the endpoint to addactivities for testing

--- front/Scripts/src/app.js
+++ front/Scripts/src/app.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var button = document.getElementById("addactivity");
-  button.addEventListener("click", function () {
-    document.body.style.backgroundColor = "orange"; // Change background color
-  });
+    var myInput = document.getElementById("myInput");
+    var button = document.getElementById('addactivity');
+    button.addEventListener('click', function () {
+        var activity = myInput.value; // Get the value from the input field
+        fetch('http://127.0.0.1:5000/createactivity', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ activity: activity }) // Convert the activity object to a JSON string
+        })
+            .then(function (response) { return response.json(); })
+            .then(function (data) {
+            console.log('Success:', data);
+        })
+            .catch(function (error) {
+            console.error('Error:', error);
+        });
+    });
 });
--- front/Scripts/src/app.ts
+++ front/Scripts/src/app.ts
@@ -1,6 +1,24 @@
+
 document.addEventListener("DOMContentLoaded", () => {
+    const myInput = document.getElementById("myInput") as HTMLInputElement;
     const button = document.getElementById('addactivity') as HTMLButtonElement;
+
     button.addEventListener('click', () => {
-        document.body.style.backgroundColor = 'orange'; // Change background color
+        const activity = myInput.value; // Get the value from the input field
+        fetch('http://127.0.0.1:5000/createactivity', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ activity: activity }) // Convert the activity object to a JSON string
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
     });
 });
+
